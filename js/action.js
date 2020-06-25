50b90c814bf557fc4a8826e15ba24effa8285d5a
thiagoof39 <thiagoof39@gmail.com>
2020-06-25 19:20:28 -0300

    Atualizações no HTML e CSS

--- js/action.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var links = document.querySelectorAll(".navbar-nav li a:not([href='#'])");
-    for (var x = 0; x < links.length; x++) {
-        links[x].onclick = function () {
-            document.querySelector("button.navbar-toggler").click();
-        }
-    }
-});
